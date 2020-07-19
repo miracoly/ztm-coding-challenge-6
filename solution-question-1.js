1688e5197dd4bf20499f977dbcb7f410161a1a03
miracoly <uhlig.marc@icloud.com>
2020-07-19 09:53:42 +0200

    Deletung Comment with the Question

--- solution-question-1.js
+++ solution-question-1.js
@@ -1,14 +1,6 @@
 /**
  * @author miracoly <https://github.com/miracoly>
  */
-
-// Question 1: Clean the room function: given an input of 
-// [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20], make a function that organizes
-// these into individual array that is ordered. For example 
-// answer(ArrayFromAbove) should return: 
-// [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591].
-// Bonus: Make it so it organizes strings differently from number types.
-// i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
 const example = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
 const answer = array => {
     // Sort array. Output: [1,1,1,1,2,2,2,4,5,10,20,20,391,392,591]
